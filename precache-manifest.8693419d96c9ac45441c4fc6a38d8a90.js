23b90cf17a2b7ffd36fbb006c28042ab1595868e
Krystian <krystian.konecki@yahoo.com>
2020-01-15 17:36:47 +0100

    Updates

---
+++ precache-manifest.8693419d96c9ac45441c4fc6a38d8a90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802e920cded9ef65c9ef193f28adea11",
+    "url": "/League-Of-Legends-Stats-REACT/index.html"
+  },
+  {
+    "revision": "63a440e631695b156922",
+    "url": "/League-Of-Legends-Stats-REACT/static/css/main.a1166b91.chunk.css"
+  },
+  {
+    "revision": "6488b0774b6881c43348",
+    "url": "/League-Of-Legends-Stats-REACT/static/js/2.9fa20a6c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/League-Of-Legends-Stats-REACT/static/js/2.9fa20a6c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63a440e631695b156922",
+    "url": "/League-Of-Legends-Stats-REACT/static/js/main.ceb09687.chunk.js"
+  },
+  {
+    "revision": "d4aee8d4652875d201f1",
+    "url": "/League-Of-Legends-Stats-REACT/static/js/runtime-main.ee3092a0.js"
+  }
+]);
